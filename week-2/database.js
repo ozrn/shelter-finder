01c74e664eb724ab5fd46e4f34668edfd29b4172
unknown <ozerenkevser92@gmail.com>
2023-06-02 16:58:11 -0400

    Read json file, make parameters dynamic

--- week-2/database.js
+++ week-2/database.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-const save = function (data) {
-    fs.writeFileSync('data.json', JSON.stringify(data))
+const save = function (filename, data) {
+    fs.writeFileSync(filename, JSON.stringify(data))
 }
 
-const load = function (data) {
-    console.log(data)
+const load = function (filename) {
+    return JSON.parse(fs.readFileSync(filename, 'utf-8'))
 }
 
 module.exports = { save, load } // exporting both of them within an object
