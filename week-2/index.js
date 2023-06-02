389f1ffe37324f19232cc96ed53aa971791a3bc3
unknown <ozerenkevser92@gmail.com>
2023-06-02 15:40:29 -0400

    Create database.js and require it in index.js

--- week-2/index.js
+++ week-2/index.js
@@ -1,15 +1,16 @@
-const Survivor = require("./survivor")
-const Shelter = require("./shelter")
-const Stocks = require("./stocks")
+const Survivor = require('./survivor')
+const Shelter = require('./shelter')
+const Stocks = require('./stocks')
+const Database = require('./database')
 
 const walter = new Survivor(12345, true)
 const lisa = new Survivor(23456, false)
 
-const eastShelter = new Shelter("East", 195, 200)
-const westShelter = new Shelter("West", 300, 300)
+const eastShelter = new Shelter('East', 195, 200)
+const westShelter = new Shelter('West', 300, 300)
 
-const foodPackage = new Stocks("Food", 100)
-const carePackage = new Stocks("Care kit", 200)
+const foodPackage = new Stocks('Food', 100)
+const carePackage = new Stocks('Care kit', 200)
 
 walter.needPlace()
 lisa.needPlace()
@@ -19,3 +20,5 @@ westShelter.checkAvailabilityFor(lisa)
 
 foodPackage.checkStocks(50)
 carePackage.checkStocks(250)
+
+Database.save(walter)
