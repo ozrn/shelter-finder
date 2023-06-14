2add9ef5ce7b77612ce201c2ed0a2ed2553b8b00
unknown <ozerenkevser92@gmail.com>
2023-06-14 16:26:07 -0400

    Create pug file, fetch data from shelter database

--- week-4/index.js
+++ week-4/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const SurvivorService = require('./services/survivor-service')
+const ShelterService = require('./services/shelter-service')
 
 const app = express()
 
@@ -14,6 +15,13 @@ app.get('/survivor/all', async (req, res) => {
     res.render('survivor', { survivors: survivors })
 })
 
+// Fetch all shelters in the specified path when they're being browsed
+app.get('/shelter/all', async (req, res) => {
+    const shelters = await ShelterService.findAll()
+    res.render('shelter', { shelters: shelters })
+})
+
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
 })
+
