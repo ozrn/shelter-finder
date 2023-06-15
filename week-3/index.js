42034facf051d53846a0fcdf5c6b2c83d43a53c1
unknown <ozerenkevser92@gmail.com>
2023-06-15 14:37:27 -0400

    Add stock-database file using stock service in index.js

--- week-3/index.js
+++ week-3/index.js
@@ -1,7 +1,9 @@
 const Survivor = require('./models/survivor')
 const Shelter = require('./models/shelter')
+const Stock = require('./models/stock')
 const SurvivorService = require('./services/survivor-service')
 const ShelterService = require('./services/shelter-service')
+const StockService = require('./services/stock-service')
 
 async function main() {
 
@@ -10,6 +12,9 @@ async function main() {
 
     const eastShelter = new Shelter('East', 200)
     const westShelter = new Shelter('West', 300)
+
+    const foodPackage = new Stock('Food', 100)
+    const carePackage = new Stock('Care kit', 200)
 
     walter.stay(eastShelter)
     lisa.stay(westShelter)
@@ -20,10 +25,15 @@ async function main() {
     await ShelterService.add(eastShelter)
     await ShelterService.add(westShelter)
 
+    await StockService.add(foodPackage)
+    await StockService.add(carePackage)
+
     const survivors = await SurvivorService.findAll()
 
 
     const shelters = await ShelterService.findAll()
+
+    const stocks = await StockService.findAll()
 
     //await SurvivorService.del(1)
 
--- week-3/models/stock.js
+++ week-3/models/stock.js
@@ -1,10 +1,14 @@
 module.exports = class Stock {
-    constructor(typeOfStock, amountOfStock) {
+    constructor(typeOfStock, amountOfStock, id) {
         this.typeOfStock = typeOfStock
         this.amountOfStock = amountOfStock
+        this.id = id
     }
     checkStocks(neededAmount) {
         ((neededAmount) - (this.amountOfStock) >= 0) ? console.log(`${this.typeOfStock} is now out of stock.`)
             : console.log(`${this.typeOfStock} is currently available.`)
+    }
+    static create({ typeOfStock, amountOfStock, id }) {
+        return new Stock(typeOfStock, amountOfStock, id)
     }
 }
