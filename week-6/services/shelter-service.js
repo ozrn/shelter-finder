ce819d766b88d13cf6ef756556466bcf1c2b0e97
unknown <ozerenkevser92@gmail.com>
2023-07-14 15:30:57 -0400

    Move method into service file

--- week-6/services/shelter-service.js
+++ week-6/services/shelter-service.js
@@ -1,8 +1,15 @@
 const BaseService = require('./base-service')
 const ShelterModel = require('../models/shelter')
+const SurvivorModel = require('../models/survivor')
 
 class ShelterService extends BaseService {
     model = ShelterModel
+
+    async showAvailableShelters() {
+        // how to get residents.length and compare it to maxCapacity for each document??
+        // return await ShelterModel.find({maxCapacity: {$gt: residents.length}})
+        return await ShelterModel.find({ maxCapacity: { $gt: 50 } })
+    }
 }
 
 module.exports = new ShelterService()
