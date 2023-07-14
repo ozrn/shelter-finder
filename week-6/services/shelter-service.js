c27998319423e535ef3f55e60513ffa4dd0456e1
unknown <ozerenkevser92@gmail.com>
2023-07-14 15:30:57 -0400

    Update method in response to fixed error

--- week-6/services/shelter-service.js
+++ week-6/services/shelter-service.js
@@ -1,8 +1,13 @@
 const BaseService = require('./base-service')
 const ShelterModel = require('../models/shelter')
+const SurvivorModel = require('../models/survivor')
 
 class ShelterService extends BaseService {
     model = ShelterModel
+
+    async showAvailableShelters() {
+        return await ShelterModel.find().$where('this.maxCapacity > this.residents.length')
+    }
 }
 
 module.exports = new ShelterService()
