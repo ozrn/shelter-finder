595f035342fd9445c369f4e1ab9f69142b609c11
unknown <ozerenkevser92@gmail.com>
2023-07-19 22:09:05 -0400

    Revert "Update function according to fixed error"
    This reverts commit 07ccd9870f156fb48a4f890b6071f5e1891e2586.

--- week-6/services/shelter-service.js
+++ week-6/services/shelter-service.js
@@ -6,7 +6,9 @@ class ShelterService extends BaseService {
     model = ShelterModel
 
     async showAvailableShelters() {
-        return await ShelterModel.find().$where('this.maxCapacity > this.residents.length')
+        // how to get residents.length and compare it to maxCapacity for each document??
+        // return await ShelterModel.find({maxCapacity: {$gt: residents.length}})
+        return await ShelterModel.find({ maxCapacity: { $gt: 50 } })
     }
 }
 
