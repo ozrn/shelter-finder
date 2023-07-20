126fa25419146913c7f1f398b4f1a1d01c26df9f
Kevser Ozeren <ozerenkevser92@gmail.com>
2023-07-19 23:15:30 -0400

    Update function in response to fixed error

--- week-6/services/shelter-service.js
+++ week-6/services/shelter-service.js
@@ -1,14 +1,8 @@
-const BaseService = require('./base-service')
-const ShelterModel = require('../models/shelter')
-const SurvivorModel = require('../models/survivor')
-
 class ShelterService extends BaseService {
     model = ShelterModel
 
     async showAvailableShelters() {
-        // how to get residents.length and compare it to maxCapacity for each document??
-        // return await ShelterModel.find({maxCapacity: {$gt: residents.length}})
-        return await ShelterModel.find({ maxCapacity: { $gt: 50 } })
+        return await ShelterModel.find().$where('this.maxCapacity > this.residents.length')
     }
 }
 
