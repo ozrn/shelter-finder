5254172b2a0b43e28c228e07251175ba949b7c01
Kevser Ozeren <ozerenkevser92@gmail.com>
2023-07-22 20:41:11 -0400

    Create app.js, move content of index.js into it

---
+++ week-7/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+require('./mongodb-connection')
+
+const survivorRouter = require('./routes/survivor')
+const shelterRouter = require('./routes/shelter')
+const stockRouter = require('./routes/stock')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.set('view engine', 'pug')
+
+app.use('/survivor', survivorRouter)
+app.use('/shelter', shelterRouter)
+app.use('/stock', stockRouter)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+module.exports = app
+
--- week-7/index.js
+++ week-7/index.js
@@ -1,25 +1,5 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-require('./mongodb-connection')
-
-const survivorRouter = require('./routes/survivor')
-const shelterRouter = require('./routes/shelter')
-const stockRouter = require('./routes/stock')
-
-const app = express()
-
-app.use(bodyParser.json())
-app.set('view engine', 'pug')
-
-app.use('/survivor', survivorRouter)
-app.use('/shelter', shelterRouter)
-app.use('/stock', stockRouter)
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
+const app = require('./app')
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000')
 })
-
