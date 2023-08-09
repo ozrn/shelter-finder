7c06e73787e7c1851f0512bb6d2a7dda17480757
Kevser Ozeren <ozerenkevser92@gmail.com>
2023-08-09 17:22:48 -0400

    Delete main page component, add form element on home page

--- week-8/frontend/src/views/HomeView.vue
+++ week-8/frontend/src/views/HomeView.vue
@@ -1,20 +1,63 @@
 <script>
 
-import MainPage from '@/components/mainpage-card.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    MainPage,
+  data() {
+    return {
+      fullName: '',
+      age: '',
+      gender: ''
+    }
   }
 }
 </script>
 
 <template>
   <div class='home'>
-    <MainPage msg='Do you need assistance?' />
+    <h2>If a survivor needs a place to stay, please fill out the form below </h2>
+    <form>
+      <label>Full Name: </label>
+      <input type="text" required v-model="fullName">
+
+      <label>Age: </label>
+      <input type="text" required v-model="age">
+
+      <label>Gender: </label>
+      <input type="text" required v-model="gender">
+    </form>
   </div>
 </template>
+
+<style>
+ form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+
+ label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6 rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+ input {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+</style>
 
 
 
