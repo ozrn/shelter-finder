e126d1ced24c331c442c826373f8a82c3998dea4
kevser özeren <ozerenkevser92@gmail.com>
2023-10-23 22:28:08 +1300

    Update mongodb url to connect to database

--- week-8/mongodb-connection.js
+++ week-8/mongodb-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-    await mongoose.connect('mongodb://localhost/needHelpNow?', { useUnifiedTopology: true, useNewUrlParser: true })
+    await mongoose.connect('mongodb://127.0.0.1/needHelpNow?', { useUnifiedTopology: true, useNewUrlParser: true })
     console.log('connected')
 }
 
