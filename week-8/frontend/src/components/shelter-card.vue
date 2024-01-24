5aec0bec99819bf20bda2804c78314c0b92d27c8
kevser özeren <ozerenkevser92@gmail.com>
2024-01-24 15:22:13 +0300

    Disable div conditionally, update style

--- week-8/frontend/src/components/shelter-card.vue
+++ week-8/frontend/src/components/shelter-card.vue
@@ -1,56 +1,60 @@
 <script>
 
 export default {
-    name: 'ShelterCard',
+  name: 'ShelterCard',
 
-    props: ['shelter'],
+  props: ['shelter'],
     
-    computed: {
-      shelterUrl() {
-        return `/shelter/${this.shelter._id}`
-      }
+  computed: {
+    shelterUrl() {
+      return `/shelter/${this.shelter._id}`
     },
-    
-    created() {
-     
+    isAvailable() {
+      return this.shelter.maxCapacity > this.shelter.residents.length
+    }
   }}
+</script>
   
-  </script>
-  
-<template lang='pug'>
-div.card
-  router-link(:to='shelterUrl') {{ shelter.name }}
-
-  <p v-if ='shelter.residents && shelter.residents.length < shelter.maxCapacity'>Available</p>
-  <p v-else>Sorry, no available place at the moment!</p>
+<template>
+ <div class="card" :class='{disabled: !isAvailable }'>
+   <router-link :to='shelterUrl'>{{ shelter.name }}</router-link>
+   <p v-if ='shelter.residents && shelter.residents.length < shelter.maxCapacity'>Available</p>
+   <p v-else>Sorry, no available place at the moment!</p>
   <hr>
-  <ul v-if = "shelter.address">
+   <ul v-if = "shelter.address">
     <li>City: {{shelter.address.city}}</li><br>
     <li >Address: {{ shelter.address.addressDetail }}</li><br>
     <li>{{ shelter.maxCapacity - shelter.residents.length }} places available</li>
-  </ul>
-
-  </template>
+   </ul>
+ </div>
+</template>
   
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  .card {
-    display: inline-block;
-    margin: 0 10px;
-    text-align: left;
-    padding: 20px;
-    border: 1px solid #ddd;
-    border-radius: 3px;
-    margin: 20px;
-    min-width: 200px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+  
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+.card {
+  display: inline-block;
+  margin: 0 10px;
+  text-align: left;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  margin: 20px;
+  min-width: 200px;
+}
+  
+a {
+  color: #42b983;
+}
+
+.disabled {
+  opacity: 0.5;
+  pointer-events: none;
+}
+</style>
