7cb4482f10d40b0cfcd0c25d52eeed32ca5e9325
kevser özeren <ozerenkevser92@gmail.com>
2024-03-06 14:32:35 +0300

    Style form

--- week-8/frontend/src/components/shelter-form.vue
+++ week-8/frontend/src/components/shelter-form.vue
@@ -1,39 +1,121 @@
 <template>
-  <div>
-    <form @submit.prevent = 'handleSubmit'>
-      <label for='type'>Type of Pet:</label>
-      <input type='text' id='type' name='type' v-model='type' placeholder='Type'><br/>
-      <label for='name'>Name:</label>
-      <input type='text' id='name' name='name' v-model='name' placeholder='Name'><br/>
-      <label for='sex'>Sex:</label>
-      <select name='sex' v-model = 'sex'>
-        <option value=''>Please select one…</option>
-        <option value='female'>Female</option>
-        <option value='male'>Male</option>
-      </select><br/>
-      <label for='age'>Age:</label>
-      <input type='text' id='age' name='age' v-model='age' placeholder='Age'><br/>
-      <label for='breed'>Breed:</label>
-      <input type='text' id='breed' name='breed' v-model='breed' placeholder='Breed'><br/>
-      <label for='microchip'>Has a microchip:</label>
-        <input type='radio' id='true' value='true' v-model='hasMicrochip'>
-        <label for='true'>Yes</label>
-        <input type='radio' id='false' value='false' v-model='hasMicrochip'>
-        <label for='false'>No</label><br/>
-      <div><button>Submit</button></div>
-    </form>   
+  <div class='container'>
+    <div class='columns is-centered is-mobile'>
+      <form @submit.prevent='handleSubmit' class='form-container'>
+        <div class='field'>
+          <div class='column'>
+            <label class='label' for='type'>Type of Pet</label>
+            <div class='control'>
+              <input
+                class='input'
+                type='text'
+                id='type'
+                name='type'
+                v-model='type'
+                placeholder='Type'
+              />
+            </div>
+          </div>
+        </div>
+        <div class='field'>
+          <div class='column'>
+            <label class='label' for='name'>Name</label>
+            <div class='control'>
+              <input
+                class='input'
+                type='text'
+                id='name'
+                name='name'
+                v-model='name'
+                placeholder='Name'
+              />
+            </div>
+          </div>
+        </div>
+        <div class='field'>
+          <div class='column'>
+            <label class='label' for='sex'>Sex</label>
+            <div class='control'>
+              <div class='select'>
+                <select name='sex' v-model='sex'>
+                  <option value=''>Please select one…</option>
+                  <option value='female'>Female</option>
+                  <option value='male'>Male</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class='field'>
+          <div class='column'>
+            <label class='label' for='age'>Age</label>
+            <div class='control'>
+              <input
+                class='input'
+                type='text'
+                id='age'
+                name='age'
+                v-model='age'
+                placeholder='Age'
+              />
+            </div>
+          </div>
+        </div>
+        <div class='field'>
+          <div class='column'>
+            <label class='label' for='breed'>Breed</label>
+            <div class='control'>
+              <input
+                class='input'
+                type='text'
+                id='breed'
+                name='breed'
+                v-model='breed'
+                placeholder='Breed'
+              />
+            </div>
+          </div>
+        </div>
+        <div class='field'>
+          <div class='column'>
+            <div class='control'>
+              <label class='radio' for='microchip'>Has a microchip</label>
+              <input
+                class='radio'
+                type='radio'
+                id='true'
+                value='true'
+                v-model='hasMicrochip'
+              />
+              <label class='radio' for='true'>Yes</label>
+              <input
+                class='radio'
+                type='radio'
+                id='false'
+                value='false'
+                v-model='hasMicrochip'
+              />
+              <label class='radio' for='false'>No</label>
+            </div>
+          </div>
+        </div>
+        <div class='field'>
+          <div class='control'>
+            <button class='button is-rounded'>Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
-</template>     
-    
-<script>
+</template>
 
+<script>
 import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'ShelterForm',
 
   data() {
-        
     return {
       type: '',
       name: '',
@@ -41,18 +123,16 @@ export default {
       age: '',
       breed: '',
       hasMicrochip: Boolean
-      }
-    },
+    }
+  },
   computed: {
     ...mapState(['shelter', 'survivor'])
   },
 
   methods: {
-
     ...mapActions(['createSurvivor', 'stayShelter']),
 
     async handleSubmit() {
-        
       let survivor = {
         type: this.type,
         name: this.name,
@@ -71,9 +151,26 @@ export default {
       await this.stayShelter(shelterInfo)
 
       await this.$router.push('/form-info')
-    } 
-  } 
-           
-}    
-    
+    }
+  }
+}
 </script>
+
+<style scoped>
+.container {
+  border: 1px solid #ddd;
+  border-radius: 0.2rem;
+  width: 24rem;
+  margin: 0 auto;
+  background-color: #e3e3f4;
+}
+
+.form-container {
+  margin: 2rem;
+}
+
+button {
+  background-color: #41418f;
+  color: #fff;
+}
+</style>
